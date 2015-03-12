b5e8418fea264f1d7566b1c98f7d4eb559d73d7f
matarn534 <matarn534@E29779.edu.linkoping.se>
2015-03-12 14:38:32 +0100

    #35 06-03-2015

--- HTML5Application/public_html/minjs.js
+++ HTML5Application/public_html/minjs.js
@@ -11,9 +11,11 @@ $(document).ready(function() {
         $(".btn").click(function() {
             $("p").remove()
 
-            rollDice(getData());
+            var diceRolled = rollDice(getData());
+            write(diceRolled);
 
         });
+        
 //hindra form från att ladda om
         $('form').submit(function(event) {
             event.preventDefault();
@@ -30,10 +32,19 @@ $(document).ready(function() {
 
 
     }//data slut
+    
+    // skriver ut värdena
+    function write (stat){
+                $.each(stat, function(index, value){
+                    $('<p>').text(value).appendTo(".resultat");
+                });
+
+    }//skriv ut slut
 
     //slumpa tal
     function rollDice(numberDice) {
         var summa = 0;
+        var stat = [];
         //loppar så många gånger man har fyllt i
         for (i = 0; i < numberDice; i++) {
 
@@ -42,12 +53,20 @@ $(document).ready(function() {
             var myNumber = Math.floor((Math.random() * 6) + 1);
             //lägger ihop summan av vad man får på varje tärning
             summa = summa + myNumber;
-            //skriver ut vad tärningen visar
-            $('<p>').text(myNumber).appendTo(".resultat");
-
+            //pusha in värdet på tärnigen i en array
+            stat.push(myNumber)
+            
+            
+           
+        
         }
-        //skriver ut summan
-        $('<p>').text(summa).appendTo(" .stat");
+        //pusha in summan av värdena på alla tärningar i arrayen
+        stat.push(summa);
+        return stat;
+        
+        
+      
+        
 
 
 
